9089eea5597fff7238ea6d3778aab928afbd474c
4eckah78 <4eckah78>
2020-11-12 23:42:25 +0300

    FIX: unreqiured manytomany fields

--- Main/models.py
+++ Main/models.py
@@ -53,5 +53,5 @@ class Mask(models.Model):
     classification = models.TextField()
     mask = models.BinaryField()
     likes = models.PositiveSmallIntegerField(default=0)
-    model = models.ManyToManyField(Model, db_table='Model_to_mask')
-    users_who_like = models.ManyToManyField(User, db_table='Likes', related_name='likes') # mb related_name='email'?
+    model = models.ManyToManyField(Model, db_table='Model_to_mask', blank=True)
+    users_who_like = models.ManyToManyField(User, db_table='Likes', related_name='likes', blank=True)
