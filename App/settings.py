e3cd1ae5d7e42a1f8b48f1e1b9acf41498c1d8f6
4eckah78 <4eckah78>
2020-11-16 19:56:01 +0300

    FIX: remove allow all host

--- App/settings.py
+++ App/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['185.243.215.18', 'code-backend.herokuapp.com', '127.0.0.1', 'localhost:3000']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
